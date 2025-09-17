1106a581543b3c120620276229c8cd1a8a40f6d9
shaurya verma <62894954+vshaurya00@users.noreply.github.com>
2025-09-18 00:56:21 +0530

    Delete app/app1.py
    placeholder deleted

--- app/app1.py
+++
@@ -1,5 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
