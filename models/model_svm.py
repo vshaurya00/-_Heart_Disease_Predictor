821f284414a05226c86836b98dd96feea6411f94
shaurya verma <62894954+vshaurya00@users.noreply.github.com>
2025-09-18 00:52:40 +0530

    Delete models/model_svm.py
    Placeholder removed

--- models/model_svm.py
+++
@@ -1,7 +0,0 @@
-📌 Next Steps
-
-Add hyperparameter tuning with Optuna / GridSearchCV
-
-Deploy Streamlit app online (Streamlit Cloud or Hugging Face Spaces)
-
-Add separate notebooks for EDA, Feature Engineering, and Modeling for clarity
